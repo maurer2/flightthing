78b99a10ba43e8f887600f8e1012b8bb99c510a4
Martin <mr.mmaurer+work@gmail.com>
2022-09-17 23:49:10 +0100

    Added tailwind and base layout

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,18 +3,25 @@ import Head from 'next/head';
 import type { NextPage } from 'next';
 // import Image from 'next/image'
 
+// https://stackoverflow.com/questions/71144514/type-error-when-using-function-with-the-return-type-nextpage
+// eslint-disable-next-line react/function-component-definition
 const Home: NextPage = () => {
   const meow = 'Meow';
 
   return (
-    <div className="">
+    <div
+      className="grid grid-rows-[auto_1fr_auto] min-h-screen bg-white text-black"
+      data-testid="app"
+    >
       <Head>
         <title>Flightthting</title>
         <meta name="description" content="Flightthting" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <header>Header {meow}</header>
+      <header className="" data-testid="app-header">
+        <h1 className="font-bold">Header {meow}</h1>
+      </header>
 
       <main className="">Main</main>
 
