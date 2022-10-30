b9941982806f7af65e57e41a3f6d3b614ed553b0
Martin <mr.mmaurer+work@gmail.com>
2022-10-30 21:30:58 +0000

    Added fallback for redux init actions

--- src/store/reducers.ts
+++ src/store/reducers.ts
@@ -4,6 +4,7 @@ import { QUERY_SENSOR, UPDATE_SENSOR } from './actionTypes';
 import { initialState } from './store';
 
 // stackoverflow.com/questions/54408912/how-to-decompose-typescript-discriminated-union-switch-block-and-keep-it-exhau
+// birukov.me/blog/all/exhaustive-type-checks.html
 function assertNever(value: never): never {
   throw new Error(`Missing value: ${value}`);
 }
@@ -22,9 +23,10 @@ const reducers = (state: NonNullable<Store> = initialState, action: Actions): St
         sensor: action.payload,
       };
     default: {
-      // assertNever(type);
-
-      console.log('type', type);
+      // allow @@redux/INIT actions to pass thru
+      if (type in action) {
+        assertNever(type);
+      }
 
       return {
         ...state,
