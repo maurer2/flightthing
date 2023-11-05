648594235e844accb513be5995244b0f8a5690ff
Martin <mr.mmaurer@gmail.com>
2023-11-05 22:06:58 +0000

    Removed sagas

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,21 +1,15 @@
 import { applyMiddleware, createStore } from 'redux';
 import logger from 'redux-logger';
-import createSagaMiddleware from 'redux-saga';
 
 import type { Store as StoreRedux } from 'redux';
 import type { SensorAction, Store } from './types';
 
 import reducers from './reducers';
-import { rootSaga } from './rootSaga';
-
-const sagaMiddleware = createSagaMiddleware();
 
 // eslint-disable-next-line @typescript-eslint/ban-ts-comment
 // @ts-ignore
 const store: StoreRedux<Store, SensorAction> & {
   dispatch: (action: SensorAction) => SensorAction;
-} = createStore(reducers, applyMiddleware(sagaMiddleware, logger));
-
-sagaMiddleware.run(rootSaga);
+} = createStore(reducers, applyMiddleware(logger));
 
 export default store;
